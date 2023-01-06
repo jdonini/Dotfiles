ba3cbf7d626d03fb109188bee1689af4368e247a
Juliano Donini <julianodonini89@gmail.com>
2023-01-06 10:36:38 -0300

    Remoção de new user

--- installer/first-install-script.sh
+++ installer/first-install-script.sh
@@ -2,17 +2,17 @@
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=us" >> /etc/vconsole.conf
-echo "archlinux" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
+# sed -i '177s/.//' /etc/locale.gen
+# locale-gen
+# echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# echo "KEYMAP=us" >> /etc/vconsole.conf
+# echo "archlinux" >> /etc/hostname
+# echo "127.0.0.1 localhost" >> /etc/hosts
+# echo "::1       localhost" >> /etc/hosts
+# echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
-# replace <PASSWORD> with chosen password
-echo root:<PASSWORD> | chpasswd
+# # replace <PASSWORD> with chosen password
+# echo root:<PASSWORD> | chpasswd
 
 pacman -S grub mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs bash-completion rsync acpi acpi_call acpid upower openbsd-netcat iptables-nft ipset firewalld flatpak os-prober ntfs-3g
 
@@ -26,12 +26,12 @@ systemctl enable firewalld
 systemctl start docker.service
 systemctl enable docker.service 
 
-# Create user juliano
-useradd -m juliano
+# # Create user juliano
+# useradd -m juliano
 
-# replace <PASSWORD> with chosen password
-echo juliano:<PASSWORD> | chpasswd
-usermod -aG wheel juliano
+# # replace <PASSWORD> with chosen password
+# echo juliano:<PASSWORD> | chpasswd
+# usermod -aG wheel juliano
 
 # change permissions for wheel group
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
